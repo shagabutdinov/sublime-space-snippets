aedb30b6210e4a3c8c652d0d4eb36b3eb9ba997d
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2014-12-23 18:02:43 +0600

    initial

---
+++ SpaceSnippets.py
@@ -0,0 +1,73 @@
+import sublime
+import sublime_plugin
+import re
+
+class InsertSpacedChar(sublime_plugin.TextCommand):
+  def run(
+    self,
+    edit,
+    char,
+    insert_space_before_regexp = None,
+    remove_space_before_regexp = None,
+    insert_space_after_regexp = None,
+    remove_space_after_regexp = None,
+  ):
+
+    selections = []
+    for sel in self.view.sel():
+      new_char = char
+      offset = sel.begin() - sel.end() + len(new_char)
+      sel.a, sel.b = sel.begin(), sel.end()
+
+      preceding_region = sublime.Region(self.view.line(sel.a).a, sel.a)
+      following_region = sublime.Region(sel.b, self.view.line(sel.b).b)
+
+      preceding_text = self.view.substr(preceding_region)
+      following_text = self.view.substr(following_region)
+
+      text = preceding_text +'__CURSOR__' + following_text
+
+      remove_space_before = (
+        remove_space_before_regexp != None and
+        re.search(remove_space_before_regexp, text) != None
+      )
+
+      insert_space_before = (
+        insert_space_before_regexp != None and
+        re.search(insert_space_before_regexp, text) != None
+      )
+
+      if remove_space_before:
+        before_space_match = re.search(r'\S(\s*)$', preceding_text)
+        if before_space_match != None:
+          before_space_length = len(before_space_match.group(1))
+          sel.a -= before_space_length
+          offset -= before_space_length
+      elif insert_space_before:
+        offset += 1
+        new_char = ' ' + new_char
+
+      remove_space_after = (
+        remove_space_after_regexp != None and
+        re.search(remove_space_after_regexp, text) != None
+      )
+
+      insert_space_after = (
+        insert_space_after_regexp != None and
+        re.search(insert_space_after_regexp, text) != None
+      )
+
+      if remove_space_after:
+        after_space_length = len(re.search(r'^(\s*)\S', following_text).group(1))
+        sel.b += after_space_length
+        offset -= after_space_length
+      elif insert_space_after:
+        new_char += ' '
+        offset += 1
+
+      self.view.replace(edit, sel, new_char)
+      sel.a, sel.b = sel.end() + offset, sel.end() + offset
+      selections.append(sel)
+
+    self.view.sel().clear()
+    self.view.sel().add_all(selections)
---
+++ readme.md
@@ -0,0 +1,69 @@
+# Sublime SpaceSnippets plugin
+
+It inserts spaces instead of you where it assumed to be inserted. Requires a lot
+of adaptation.
+
+There is not much help from this plugin in writing code but when I personally
+started to used it I liked it and prefer to write code with this plugin rather
+than without.
+
+
+### Features
+
+Provides spaces insertion for certain characters. Spaces style are adapted to my
+personal coding style so it can be worthless for others.
+
+Example:
+
+# type "myvar"
+myvar| # <- cursor is |
+
+# type "+"
+myvar+|
+
+# plugin will convert "+" to " + " (with spaces)
+myvar + |
+
+# some special cases are handled too:
+# type "myvar":
+myvar| # <- cursor is |
+
+# than type "+"
+MyClass + |
+
+# than type "=" (automatically removes space between "+" and "=")
+MyClass += |
+
+
+### Installation
+
+This plugin is part of [sublime-enhanced](http://github.com/shagabutdinov/sublime-enhanced)
+plugin set. You can install sublime-enhanced and this plugin will be installed
+automatically.
+
+If you would like to install this package separately check "Installing packages
+separately" section of [sublime-enhanced](http://github.com/shagabutdinov/sublime-enhanced)
+package.
+
+If you don't like fuzzy behavior you should rebind keyboard shortcuts after
+installation in the "Autocompletion/Default (OSNAME).sublime-keymap" file
+(non-fuzzy behavior are commented by default).
+
+
+### Commands
+
+| Description       | Keyboard shortcuts |
+|-------------------|--------------------|
+| Insert spaced "," | ,                  |
+| Insert spaced ":" | :                  |
+| Insert spaced ">" | >                  |
+| Insert spaced "<" | <                  |
+| Insert spaced "*" | *                  |
+| Insert spaced "/" | /                  |
+| Insert spaced "+" | +                  |
+| Insert spaced "-" | -                  |
+| Insert spaced "&" | &                  |
+| Insert spaced "|" | |                  |
+| Insert spaced "=" | =                  |
+| Insert spaced "!" | !                  |
+| Insert spaced "." | .                  |
